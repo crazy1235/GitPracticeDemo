4eb344e6ebee6a970597d0dad849fb935d1f06a9
yansen <jacksen1992@163.com>
2017-10-20 23:09:55 -0500

    Update Constants.java
    Constants.java添加常量

--- app/src/main/java/com/jacksen/gitpractice/Constants.java
+++ app/src/main/java/com/jacksen/gitpractice/Constants.java
@@ -10,9 +10,14 @@ public class Constants {
      * app版本号
      */
     public static final int APP_VERSION = 1;
-    /*
+    /**
      * 全局tag
      */
     public static final String TAG = "Git.demo";
+    
+    /**
+     * 当前设备类型
+     */
+    public static final String OS_TYPE = "Android";
 
 }
